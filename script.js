ac3c01d64ed914df46d873ad25c6b5db7f001844
Aaron Long <aaronlng05@gmail.com>
2020-05-28 08:34:01 -0400

    make it act like a game part 1

--- script.js
+++ script.js
@@ -4,15 +4,38 @@ const doorImage3 = document.getElementById('door3');
 const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+const numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+const randomChoreDoorGenerator = () => {
+    const choreDoor = Math.floor(Math.random() * numClosedDoors);
+    if (choreDoor === 0) {
+        openDoor1 = botDoorPath;
+        openDoor2 = beachDoorPath;
+        openDoor3 = spaceDoorPath;
+    } else if (choreDoor === 1) {
+        openDoor2 = botDoorPath;
+        openDoor3 = beachDoorPath;
+        openDoor1 = spaceDoorPath;
+    } else if (choreDoor === 2) {
+        openDoor3 = botDoorPath;
+        openDoor1 = beachDoorPath;
+        openDoor2 = spaceDoorPath;
+    }
+}
 
 doorImage1.onclick = () => {
-    doorImage1.src = botDoorPath;
+    doorImage1.src = openDoor1;
 };
 
 doorImage2.onclick = () => {
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
 };
 
 doorImage3.onclick = () => {
-    doorImage3.src = spaceDoorPath;
+    doorImage3.src = openDoor3;
 };
+
+randomChoreDoorGenerator();
